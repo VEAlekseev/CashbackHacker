c2044bac176716e4b49264672227621d8fdf6efe
Vladimir Alekseev <devilspb92@mail.ru>
2019-12-12 18:52:41 +0300

    added Parameterized test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,34 +1,17 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-    @Test
-    void verifyPaymentOn900() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/Pay.csv", numLinesToSkip = 1)
+    void verifyPayment(int amount, int expected, String message) {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
         int actual = cashbackHackService.remain(amount);
-        int expected = 100;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void verifyPaymentOn1100() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1100;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 900;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void verifyPaymentOn1000() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 0;
         assertEquals(expected, actual);
     }
 }
